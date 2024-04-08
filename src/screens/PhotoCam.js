7b4b944a8ca77dd71364bfec0587b0dce0083d39
Eduardo <cesconettoedu@gmail.com>
2024-04-08 10:56:47 -0400

    del branch

--- src/screens/PhotoCam.js
+++ src/screens/PhotoCam.js
@@ -1,9 +1,34 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function PhotoCam() {
+
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    return <Text>No permission to camera</Text>;
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>PhotoCam</Text>
+      <View style={styles.cameraSize}>
+
+      </View>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} 
+            onPress={toggleCameraType}
+          >
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -11,8 +36,29 @@ export default function PhotoCam() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7f5f4',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  cameraSize: {
+    width: 200,
+    height: 200
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
