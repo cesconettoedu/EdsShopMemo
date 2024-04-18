fb33f9b3f83d05854c38d3df6b043a6a657cb845
Eduardo <cesconettoedu@gmail.com>
2024-04-18 14:40:14 -0400

    add gif load on welcome

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,24 +1,50 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Image} from "react-native";
 
+import EuIcon from "../../assets/icons/eulogoSquareTodo.png";
+import Load from "../../assets/gif/load.gif";
 
 function Welcome({navigation}) {
 
   const justEnter = () => {
     setTimeout(() => {
       navigation.navigate('Home')
-    }, 2000);
+    }, 1000);
   };
 
   useEffect(() => {
-    justEnter();
+     justEnter();
   }, [])
 
   return (
-    <>
-      <Text>Welcome</Text>
-    </>
+    <View style={styles.container}>
+      <Image
+        source={EuIcon}
+        alt="euicon"
+        style={{width: 150, height: 150}}
+      />
+      <Image
+        source={Load}
+        alt="loading"
+        style={{width: 150, height: 150}}
+      />
+      
+    </View>
   )
 }
 
 export default Welcome
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    padding: 15
+  }, 
+  text: {
+    color: '#fd7014',
+    fontSize: 24,
+  }
+})
